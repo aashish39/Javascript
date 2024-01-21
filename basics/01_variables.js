e29919cda5ae442f5275736f6fdd00ad1e934376
Ashish Shivnath Deshmukh <67585084+aashish39@users.noreply.github.com>
2024-01-21 09:57:02 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "ashish@amazon.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+/*
+    Prefer Not To use var
+*/
+// accountId = 2
+console.log(accountId);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS Code as newer version
+// alert(5+4); We are using node js not browser
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age);
